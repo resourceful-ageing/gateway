2ac8d05a8d2dab4e0089387f54ef464d8662c8b8
Rob van Gansewinkel <rob.van.gansewinkel@philips.com>
2017-03-15 14:50:08 +0100

    added reboot/update scripts

---
+++ scripts/reboot.js
@@ -0,0 +1,20 @@
+var Client = require("ibmiotf");
+var appClientConfig = {
+    "org" : "6tv4n6",
+    "id" : "e4f334ba-e275-483a-8b43-0c51d9efbc94",
+    "domain": "internetofthings.ibmcloud.com",
+    "auth-key" : "a-6tv4n6-wgxnvgbyhr",
+    "auth-token" : "wMA8?KklIsjPEh9SAn"
+}
+
+
+var appClient = new Client.IotfApplication(appClientConfig);
+
+appClient.connect();
+ 
+appClient.on("connect", function () {
+    var myData={'DelaySeconds' : 10};
+    myData = JSON.stringify(myData);
+    appClient.publishDeviceCommand("gateway","b827eb28efa7", "reboot", "json", myData);
+    console.log("send command reboot"); 
+});
