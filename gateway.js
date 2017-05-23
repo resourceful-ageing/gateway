d6dc6e1000b93227472c6f5841d80e11afc8c378
kc1212 <kc1212@users.noreply.github.com>
2017-05-23 14:50:12 +0200

    Convert line ending

--- gateway.js
+++ gateway.js
@@ -1,19 +1,6 @@
-/*
- * Example code of read sensor data of multiple SensorTag (CC2541DK-SENSOR, CC2650STK) at same time by using node-sensortag
- *
- * Node.js 0.10.36 or higher (4.4.2 ready)
- *
- * Usage;
- * $ npm install sensortag async
- * $ node multiple_sensortag.js
- *
- */
-
 var shell = require('shelljs');
-var exec = require('child_process').exec;
 var SensorTag = require('./local_modules/sensortag');
 var async = require('async');
-var url = require('url');
 var macUtil = require('getmac');
 var properties = require('properties');
 var ibmClient = require('ibmiotf');
@@ -32,69 +19,69 @@ var mqttService = (function () {
   function createInstance() {
     var object = {};
 
-    object.connect = function() {
+    object.connect = function () {
 
-      properties.parse('./config.properties', {path: true}, function(err, cfg) {
+      properties.parse('./config.properties', { path: true }, function (err, cfg) {
         console.log('MQTT: loading credentials');
         if (err) {
           console.error('A file named config.properties containing the device registration from the IBM IoT Cloud is missing.');
           console.error('The file must contain the following properties: org, type, id, auth-token.');
-          throw e;
+          throw err;
         }
-        
-        macUtil.getMac(function(err, macAddress) {
+
+        macUtil.getMac(function (err, macAddress) {
           if (err) throw err;
-          
+
           var deviceId = macAddress.replace(/:/gi, '');
           console.log('MQTT: device MAC Address: ' + deviceId);
 
-          if(cfg.id != deviceId) {
+          if (cfg.id != deviceId) {
             console.warn('The device MAC address does not match the ID in the configuration file.');
           }
-          
+
           var config = {
-            "org"   : cfg['org'],
-            "type"  : cfg['type'],
-            "id"    : cfg['id'],
+            "org": cfg['org'],
+            "type": cfg['type'],
+            "id": cfg['id'],
             "domain": "internetofthings.ibmcloud.com",
-            "auth-method" : cfg['auth-method'],
-            "auth-token"  : cfg['auth-token'],
+            "auth-method": cfg['auth-method'],
+            "auth-token": cfg['auth-token'],
             "use-client-certs": false
           };
-          
+
           client = new ibmClient.IotfGateway(config);
-          
+
           client.connect();
-          
-          client.on('connect', function(){
+
+          client.on('connect', function () {
             console.log('IBM: gateway client connected');
             connected = true;
 
             client.subscribeToGatewayCommand('reboot-gateway');
-	    client.subscribeToGatewayCommand('update-gateway');
+            client.subscribeToGatewayCommand('update-gateway');
             client.subscribeToGatewayCommand('list-sensors');
           });
 
-          client.on('command', function(type, id, commandName, commandFormat, payload, topic) {
-	    switch(commandName) {
+          client.on('command', function (type, id, commandName, commandFormat, payload, topic) {
+            switch (commandName) {
               case 'reboot-gateway':
-		shell.exec('reboot');
-		break;
-	      case 'update-gateway':
-		if (shell.exec('sudo git reset --hard').code === 0) {
+                shell.exec('reboot');
+                break;
+              case 'update-gateway':
+                if (shell.exec('sudo git reset --hard').code === 0) {
                   console.log('updated, send event');
                   client.publishDeviceEvent("sensortag", deviceId, "gateway-updated", "json", {});
-		}
-		break;
-	      case 'list-sensors':
+                }
+                break;
+              case 'list-sensors':
 
-		  console.log('return list of sensors ('+Object.keys(devices).length+ ' connected devices)');
-		  console.log(devices);
-		  client.publishDeviceEvent("sensortag", deviceId, "sensors-listed", "json", { "devices": JSON.stringify(devices) });
-		break;
-	    }
+                console.log('return list of sensors (' + Object.keys(devices).length + ' connected devices)');
+                console.log(devices);
+                client.publishDeviceEvent("sensortag", deviceId, "sensors-listed", "json", { "devices": JSON.stringify(devices) });
+                break;
+            }
           });
-          
+
           client.on("error", function (err) {
             console.log('IBM: ' + err);
             connected = false;
@@ -105,12 +92,12 @@ var mqttService = (function () {
         });
       });
     }
-    
-    object.isConnected = function() {
+
+    object.isConnected = function () {
       return connected;
     }
 
-    object.getClient = function() {
+    object.getClient = function () {
       return client;
     }
 
@@ -127,26 +114,26 @@ var mqttService = (function () {
   };
 })();
 
-var onDiscover = function(sensorTag) {
+var onDiscover = function (sensorTag) {
   console.info('Sensortag: start discovery');
 
-  sensorTag.once('disconnect', function() {
+  sensorTag.once('disconnect', function () {
     clearInterval(device_timers[sensorTag.id]);
-    delete(device_timers[sensorTag.id]);
-    delete(devices[sensorTag.id]);
+    delete (device_timers[sensorTag.id]);
+    delete (devices[sensorTag.id]);
     console.info('Sensortag: ' + sensorTag.id + ' disconnected (' + Object.keys(devices).length + ' connected)');
   });
 
   async.series({
-    connectAndSetUp: function(next) {
+    connectAndSetUp: function (next) {
       console.info('Sensortag: ' + sensorTag.id + ' discovered');
-      sensorTag.connectAndSetUp(function() {
+      sensorTag.connectAndSetUp(function () {
         SensorTag.discover(onDiscover); // NOTE: resume for discover other devices
         next();
       });
     },
-    enableSensors: function(next) {
-      
+    enableSensors: function (next) {
+
       try {
         // environmental data
         sensorTag.enableIrTemperature();
@@ -159,123 +146,123 @@ var onDiscover = function(sensorTag) {
         sensorTag.enableAccelerometer();
         sensorTag.enableGyroscope();
         sensorTag.enableMagnetometer();
-        
+
         sensorTag.notifyAccelerometer();
         sensorTag.notifyGyroscope();
         sensorTag.notifyMagnetometer();
 
-      } catch(ex) {
+      } catch (ex) {
         // NOTE: Ignored because not supported
       }
       devices[sensorTag.id] = moment().unix();
       console.info('Sensortag: ' + sensorTag.id + ' ready (' + Object.keys(devices).length + ' connected)');
       next();
     },
-  }, function() {
-      // NOTE: In case of polling in periodic
-      device_timers[sensorTag.id] = setInterval(function() {
-        async.parallel({
-          Info: function(next) {
-            var info = {id: sensorTag.id, type: sensorTag.type};
-            sensorTag._peripheral.updateRssi(function(error, rssi) {
-              info.rssi = rssi;
+  }, function () {
+    // NOTE: In case of polling in periodic
+    device_timers[sensorTag.id] = setInterval(function () {
+      async.parallel({
+        Info: function (next) {
+          var info = { id: sensorTag.id, type: sensorTag.type };
+          sensorTag._peripheral.updateRssi(function (error, rssi) {
+            info.rssi = rssi;
+          });
+          next(null, info);
+        },
+        Humidity: function (next) {
+          sensorTag.readHumidity(function (error, temperature, humidity) {
+            next(null, { temperature: temperature, humidity: humidity });
+          });
+        },
+        Barometer: function (next) {
+          try {
+            sensorTag.readBarometricPressure(function (error, pressure) {
+              next(null, { pressure: pressure });
             });
-            next(null, info);
-          },
-          Humidity: function(next) {
-            sensorTag.readHumidity(function(error, temperature, humidity) {
-              next(null, {temperature: temperature, humidity: humidity});
-            });
-          },
-          Barometer: function(next) {
-            try {
-              sensorTag.readBarometricPressure(function(error, pressure) {
-                next(null, {pressure: pressure});
-              });
-            } catch(ex) {
-              next(); // NOTE: Ignored because not supported
-            };
-          },
-          Luxometer: function(next) {
-            try {
-              sensorTag.readLuxometer(function(error, lux) {
-                next(null, {lux: lux});
-              });
-            } catch(ex) {
-              next(); // NOTE: Ignored because not supported
-            };
-          }
-        }, function(err, data) {
-           
-          var newData = {
-            "d": {
-              "rssi": data.Info.rssi,
-              "pressure" : data.Barometer.pressure,
-              "humidity" : data.Humidity.humidity,
-              "temperature" : data.Humidity.temperature,
-              "lux" : data.Luxometer.lux
-            }
+          } catch (ex) {
+            next(); // NOTE: Ignored because not supported
           };
-          console.log(newData);
+        },
+        Luxometer: function (next) {
+          try {
+            sensorTag.readLuxometer(function (error, lux) {
+              next(null, { lux: lux });
+            });
+          } catch (ex) {
+            next(); // NOTE: Ignored because not supported
+          };
+        }
+      }, function (err, data) {
+
+        var newData = {
+          "d": {
+            "rssi": data.Info.rssi,
+            "pressure": data.Barometer.pressure,
+            "humidity": data.Humidity.humidity,
+            "temperature": data.Humidity.temperature,
+            "lux": data.Luxometer.lux
+          }
+        };
+        console.log(newData);
+        devices[sensorTag.id] = moment().unix();
+
+        if (mqttService.getInstance().isConnected()) {
+          mqttService.getInstance().getClient().publishDeviceEvent('sensortag', sensorTag.id, 'air', 'json', JSON.stringify(newData));
+        }
+      });
+    }, polling_interval);
+
+    // NOTE: In case of listening for notification
+
+    sensorTag.on('accelerometerChange', function (x, y, z) {
+      if (x != 0 || y != 0 || z != 0) {
+        var newData = {
+          "d": {
+            "x": x,
+            "y": y,
+            "z": z
+          }
+        };
+        //console.log(sensorTag.id);
+        console.log(newData);
+
+        if (mqttService.getInstance().isConnected()) {
           devices[sensorTag.id] = moment().unix();
-
-          if (mqttService.getInstance().isConnected()) {
-            mqttService.getInstance().getClient().publishDeviceEvent('sensortag', sensorTag.id, 'air', 'json', JSON.stringify(newData));
-          }
-        });
-      }, polling_interval);
-
-      // NOTE: In case of listening for notification
-
-      sensorTag.on('accelerometerChange', function(x, y, z) {
-        if (x != 0 || y != 0 || z != 0) {
-          var newData = {
-            "d": {
-              "x": x,
-              "y": y,
-              "z": z
-            }
-          };
-//console.log(sensorTag.id);
-console.log(newData);
-
-          if (mqttService.getInstance().isConnected()) {
-            devices[sensorTag.id] = moment().unix();
-            mqttService.getInstance().getClient().publishDeviceEvent('sensortag', sensorTag.id, 'accel', 'json', JSON.stringify(newData));
-          }
+          mqttService.getInstance().getClient().publishDeviceEvent('sensortag', sensorTag.id, 'accel', 'json', JSON.stringify(newData));
         }
-      });
-      sensorTag.on('gyroscopeChange', function(x, y, z) {
-        if (x != 0 || y != 0 || z != 0) {
-          var newData = {
-            "d": {
-              "x": x,
-              "y": y,
-              "z": z
-            }
-          };
-          if (mqttService.getInstance().isConnected()) {
-            devices[sensorTag.id] = moment().unix();
-            mqttService.getInstance().getClient().publishDeviceEvent('sensortag', sensorTag.id, 'gyro', 'json', JSON.stringify(newData));
+      }
+    });
+    sensorTag.on('gyroscopeChange', function (x, y, z) {
+      if (x != 0 || y != 0 || z != 0) {
+        var newData = {
+          "d": {
+            "x": x,
+            "y": y,
+            "z": z
           }
+        };
+        if (mqttService.getInstance().isConnected()) {
+          devices[sensorTag.id] = moment().unix();
+          mqttService.getInstance().getClient().publishDeviceEvent('sensortag', sensorTag.id, 'gyro', 'json', JSON.stringify(newData));
         }
-      });
-      sensorTag.on('magnetometerChange', function(x, y, z) {
-        if (x != 0 || y != 0 || z != 0) {
-          var newData = {
-            "d": {
-              "x": x,
-              "y": y,
-              "z": z
-            }
-          };
-          if (mqttService.getInstance().isConnected()) {
-            devices[sensorTag.id] = moment().unix();
-            mqttService.getInstance().getClient().publishDeviceEvent('sensortag', sensorTag.id, 'mag', 'json', JSON.stringify(newData));
+      }
+    });
+    sensorTag.on('magnetometerChange', function (x, y, z) {
+      if (x != 0 || y != 0 || z != 0) {
+        var newData = {
+          "d": {
+            "x": x,
+            "y": y,
+            "z": z
           }
+        };
+        if (mqttService.getInstance().isConnected()) {
+          devices[sensorTag.id] = moment().unix();
+          mqttService.getInstance().getClient().publishDeviceEvent('sensortag', sensorTag.id, 'mag', 'json', JSON.stringify(newData));
         }
-      });
-    }
+      }
+    });
+  }
   );
 };
 
